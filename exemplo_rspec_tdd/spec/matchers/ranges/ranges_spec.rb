aee55c17291ec8193ea119b54a1de43b6034aa21
wilfrank84 <wilfrank84@gmail.com>
2017-12-01 21:59:56 -0200

    Subject onliner syntax

--- exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -4,4 +4,9 @@ describe (1..5), 'Ranges' do
     expect(subject).to cover(1,2)
     expect(subject).not_to cover(0,6)
   end
+
+  it { is_expected.to cover(1) }
+  it { is_expected.to cover(1,2) }
+  it { is_expected.not_to cover(0,6) }
+  it { expect(subject).to cover(3) }
 end
