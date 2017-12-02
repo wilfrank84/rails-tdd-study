d0df928b0a606b8edbcc789c4ff1928b2a9e8647
wilfrank84 <wilfrank84@gmail.com>
2017-12-01 22:11:44 -0200

    composicao de expectativas

---
+++ exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
@@ -0,0 +1,8 @@
+describe 'Ruby on Rails' do
+  it { is_expected.to start_with('Ruby').and end_with('Rails') }
+  it { expect(fruta).to eq('banana').or eq('laranja').or eq('maca') }
+
+  def fruta
+    %w(banana laranja maca).sample
+  end
+end
