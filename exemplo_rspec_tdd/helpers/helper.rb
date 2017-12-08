b9b8ab99fdc61ba829c9b18d96e874699fdc01ac
wilfrank84 <wilfrank84@gmail.com>
2017-12-07 23:01:03 -0200

    helper methods

---
+++ exemplo_rspec_tdd/helpers/helper.rb
@@ -0,0 +1,5 @@
+module Helper
+  def fruta # helper method arbitrário
+    %w(banana laranja maca).sample
+  end
+end
--- exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
+++ exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
@@ -1,8 +1,9 @@
+
 describe 'Ruby on Rails' do
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
   it { expect(fruta).to eq('banana').or eq('laranja').or eq('maca') }
 
-  def fruta
-    %w(banana laranja maca).sample
-  end
+  # def fruta # helper method arbitrário
+  #   %w(banana laranja maca).sample
+  # end
 end
